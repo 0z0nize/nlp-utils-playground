99945e82b2196eabc94b3258d3d07fce5c763b81
Vladislav Shkarovskiy <112638163+0z0nize@users.noreply.github.com>
2026-05-31 10:51:12 +0000

    docs: expand docstring for read_jsonl

--- utils/read_jsonl.py
+++ utils/read_jsonl.py
@@ -4,3 +4,7 @@ import json
 def read_jsonl(path):
     with open(path) as f:
         return [json.loads(line) for line in f]
+
+# Example:
+#   from utils.read_jsonl import *
+#   # see tests/ for usage
