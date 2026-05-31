9c25c69268edee131d6f801e6e9f410a0f03dc41
Vladislav Shkarovskiy <112638163+0z0nize@users.noreply.github.com>
2026-05-31 10:12:04 +0000

    refactor: add levenshtein distance

---
+++ utils/levenshtein.py
@@ -0,0 +1,11 @@
+"""Levenshtein distance."""
+
+def lev(a, b):
+    if len(a) < len(b): a, b = b, a
+    prev = list(range(len(b)+1))
+    for i, ca in enumerate(a, 1):
+        cur = [i]
+        for j, cb in enumerate(b, 1):
+            cur.append(min(cur[-1]+1, prev[j]+1, prev[j-1]+(ca!=cb)))
+        prev = cur
+    return prev[-1]
