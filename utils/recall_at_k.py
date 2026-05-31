65a610d6e49b0fb9f769b563bd22d5d34a236356
Vladislav Shkarovskiy <112638163+0z0nize@users.noreply.github.com>
2026-05-31 10:50:05 +0000

    docs: expand docstring for recall_at_k

--- utils/recall_at_k.py
+++ utils/recall_at_k.py
@@ -2,3 +2,7 @@
 
 def r_at_k(relevant, retrieved, k):
     return len(set(retrieved[:k]) & set(relevant)) / len(relevant) if relevant else 0
+
+# Example:
+#   from utils.recall_at_k import *
+#   # see tests/ for usage
