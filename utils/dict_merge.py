38a0b97c320fced17bd70de8a6934795fa3ba246
Vladislav Shkarovskiy <112638163+0z0nize@users.noreply.github.com>
2026-05-31 10:15:09 +0000

    chore: add recursive dict merge

---
+++ utils/dict_merge.py
@@ -0,0 +1,9 @@
+"""Recursive dict merge."""
+
+def merge(a, b):
+    out = dict(a)
+    for k, v in b.items():
+        if k in out and isinstance(out[k], dict) and isinstance(v, dict):
+            out[k] = merge(out[k], v)
+        else: out[k] = v
+    return out
