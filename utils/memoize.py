0fc4657ad0345a3eaa88710d437c69375da39a4a
Vladislav Shkarovskiy <112638163+0z0nize@users.noreply.github.com>
2026-05-31 10:55:04 +0000

    docs: expand docstring for memoize

--- utils/memoize.py
+++ utils/memoize.py
@@ -6,3 +6,7 @@ def memo(fn):
         if args not in cache: cache[args] = fn(*args)
         return cache[args]
     return wrap
+
+# Example:
+#   from utils.memoize import *
+#   # see tests/ for usage
