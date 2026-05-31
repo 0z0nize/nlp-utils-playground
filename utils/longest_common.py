4916e72b05273a286479f76e1d91b6fc8c62b54f
Vladislav Shkarovskiy <112638163+0z0nize@users.noreply.github.com>
2026-05-31 10:53:02 +0000

    docs: expand docstring for longest_common

--- utils/longest_common.py
+++ utils/longest_common.py
@@ -6,3 +6,7 @@ def lcs(a, b):
         for j in range(i+1, len(a)+1):
             if a[i:j] in b and len(a[i:j]) > len(best): best = a[i:j]
     return best
+
+# Example:
+#   from utils.longest_common import *
+#   # see tests/ for usage
