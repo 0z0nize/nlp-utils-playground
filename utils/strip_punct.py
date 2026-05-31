8916468b20b512b7799e268b3cfe7b4a3f6a89d1
Vladislav Shkarovskiy <112638163+0z0nize@users.noreply.github.com>
2026-05-31 10:50:59 +0000

    docs: expand docstring for strip_punct

--- utils/strip_punct.py
+++ utils/strip_punct.py
@@ -3,3 +3,7 @@
 import string
 def strip_punct(text):
     return text.translate(str.maketrans('', '', string.punctuation))
+
+# Example:
+#   from utils.strip_punct import *
+#   # see tests/ for usage
